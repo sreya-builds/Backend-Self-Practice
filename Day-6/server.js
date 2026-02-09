b6723df13aa450faefda2b613896d1d0418f8ada
Shreya <sreya-builds@users.noreply.github.com>
2026-02-09 13:42:56 +0530

    Day-6 and Day 7

---
+++ Day-6/server.js
@@ -0,0 +1,13 @@
+require("dotenv").config()
+
+const app = require("./src/app")
+
+const mongoose  = require("mongoose")
+const connectToDb = require("./src/config/database")
+
+ connectToDb()
+
+app.listen(3000 , ()=>{
+    console.log("server is running");
+    
+})
